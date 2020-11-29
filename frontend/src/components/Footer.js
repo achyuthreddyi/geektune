8c01b86fff68dc0d9b925552a4acbbd6d45ab692
achyuth reddy <ncsachyuth@gmail.com>
2020-11-29 10:29:57 +0530

    created the basic footer for playing songs

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,15 +1,89 @@
-import React from 'react'
-import { Container, Col } from 'react-bootstrap'
+import React, {useEffect, useState} from 'react'
 
-const Footer = () => {
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
+import PauseCircleOutlineIcon from "@material-ui/icons/PauseCircleOutline";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
+import axios from 'axios'
+
+import "./Footer.css"
+import { Grid, Slider } from '@material-ui/core';
+// import { useDataLayerValue } from '../../context/DataLayer';
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
+
+function Footer( { spotify } ) {
+
+    // const [ { token, item, playing }, dispatch ] = useDataLayerValue()
+    const [song, setSong] = useState('')
+
+    useEffect(() => {
+      const getSong = async() =>{
+        const {data} = await axios.get('http://localhost:5000/5fb25f29bed39fa8b1d8a723')
+        setSong(data)
+        console.log('data of the song',data);
+      }
+      getSong()      
+
+   
+    }, [])
+
     return (
-        <footer>
-            <Container>
-                <Col className='text-center py-3'>
-                    CopyRight &copy; AchyuthReddy
-                </Col>
-            </Container>
-        </footer>       
+
+        <div className="footer">
+        <div className="footer__left">
+          {/* <img
+            className="footer__albumLogo"
+            src={item?.album.images[0].url}
+            alt={item?.name}
+          /> */}
+            <img className= "footer__albumLogo"
+                src = {song.imageUrl}
+
+                alt=""/>
+          {song ? (
+            <div className="footer__songInfo">
+              <h4>{song.name}</h4>
+              <p>{song.artist}</p>
+            </div>
+          ) : (
+            <div className="footer__songInfo">
+              <h4>No song is playing</h4>
+              <p>...</p>
+            </div>
+          )}
+        </div>
+        <div className="footer__center">
+          <AudioPlayer
+            autoPlay
+            src="http://localhost:5000/songPlay/5fb25f29bed39fa8b1d8a723"
+            onPlay={e => console.log("onPlay")}
+            // other props here
+          />  
+
+          
+        </div>
+        
+        <div className="footer__right">
+          <div>
+          <SkipNextIcon/>
+          <SkipPreviousIcon />
+          
+
+          </div>
+          
+
+          
+        </div>
+        
+        
+      </div>
+  
+       
     )
 }
 
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
 
 const HomeScreen = () => {
   return (
     <div>
-      <h1>Home Screen</h1>
-      
+      <h1>Home Screen</h1>    
+      <AudioPlayer
+          autoPlay
+          src="http://localhost:5000/songPlay/5fb25f29bed39fa8b1d8a723"
+          onPlay={e => console.log("onPlay")}
+          // other props here
+        />  
     </div>
   )
 }
